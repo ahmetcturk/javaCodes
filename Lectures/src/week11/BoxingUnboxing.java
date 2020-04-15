bb0e61828835bab0aed71523a67692ca15821446
siliconelabs <siliconelabs@192.168.1.18>
2020-04-14 20:03:19 -0400

    class

---
+++ Lectures/src/week11/BoxingUnboxing.java
@@ -0,0 +1,18 @@
+package week11;
+
+public class BoxingUnboxing {
+
+	public static void main(String[] args) {
+		
+	int a = 50; 
+	
+	// Boxing
+	Integer integer = a;
+	System.out.println(integer);
+	
+	Integer integer2 = 100;
+	int b = integer2;
+	System.out.println(b);
+	}
+
+}
